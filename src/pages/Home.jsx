59f022ccac22a1393043a31d73dfea1ccd5328b9
Omar1H1 <omerhamad1o1@gmail.com>
2024-02-01 14:26:21 +0100

    mod*

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,12 @@ import Header from '../components/Header';
 import Features from '../components/Features';
 import Footer from '../components/Footer';
 import Offers from '../components/Offers';
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
 
 export default function Home() {
   return (<>
     <Header />
-    <Offers></Offers>
+    <Offers />
     <main>
       <Features />
     </main>
